fc28ac0f038dbe7384994aa9ed16741fe76f36e6
Oussama Bouanani <oussama.bouanani@protonmail.com>
2019-07-13 14:13:57 +0200

    Add new Dialogue scriptable object

---
+++ MiniGameJam.07.2019/Assets/Dialogue System/Scripts/Dialogue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu (fileName = "newDialogue", menuName = "MiniJam.07.2019/Dialogue")]
+public class Dialogue : ScriptableObject
+{
+    //message to appear in the dialogue
+    [SerializeField]
+    private string message = "MESSAGE";
+    public string GetMessage () { return message; }
+
+    //list of dialogue options:
+    [System.Serializable]
+    public struct Option
+    {
+        public bool status; //is this the right/wrong answer?
+        public string message; //the dialogue's option answer
+        public UnityEvent triggerEvent;
+    }
+    [SerializeField]
+    private List<Option> options = new List<Option>();
+    public IEnumerable<Option> GetOptions () { return options; }
+}
