9f73c665848f71e6b183f2f2adcf9ad59866ec47
Oussama Bouanani <oussama.bouanani@protonmail.com>
2019-07-13 16:19:32 +0200

    Add UI for lives and required answers

--- MiniGameJam.07.2019/Assets/GameSystem.cs
+++ MiniGameJam.07.2019/Assets/GameSystem.cs
@@ -1,15 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameSystem : MonoBehaviour
 {
     public static GameSystem instance = null; //only one instance per game
 
     [SerializeField]
-    public int lives = 3; //amount of lives the player has
+    private Image imagePrefab = null;
+
+    [System.Serializable]
+    public struct UIGroup
+    {
+        public Sprite sprite;
+        public Stack<Image> instances;
+        public GridLayoutGroup parent;
+
+        public void Init(int amount, Image prefab)
+        {
+            instances = new Stack<Image>();
+
+            for(int i = 0; i < amount; i++)
+            {
+                Image newImage = Instantiate(prefab);
+                newImage.sprite = sprite;
+                newImage.transform.SetParent(parent.transform, true);
+
+                instances.Push(newImage);
+            }
+        }
+
+        public void Decrement ()
+        {
+            if (instances.Count <= 0)
+                return;
+
+            Image remImage = instances.Pop();
+            remImage.gameObject.SetActive(false);
+        }
+    }
+
     [SerializeField]
-    public int requiredAnswers = 5; //the required correct answers in order to proceed to the next level
+    private int lives = 3; //amount of lives the player has
+    [SerializeField]
+    private UIGroup livesUI = new UIGroup();
+
+    [SerializeField]
+    private int requiredAnswers = 5; //the required correct answers in order to proceed to the next level
+    [SerializeField]
+    private UIGroup answersUI = new UIGroup();
 
     private void Awake()
     {
@@ -17,6 +57,10 @@ public class GameSystem : MonoBehaviour
             instance = this;
         else if (instance != this)
             Destroy(instance);
+
+        //init the lives and required UI code:
+        livesUI.Init(lives, imagePrefab);
+        answersUI.Init(requiredAnswers, imagePrefab);
     }
 
     //called when the player answers a dialogue
@@ -25,6 +69,8 @@ public class GameSystem : MonoBehaviour
         if(correct) //if this is a correct answer
         {
             requiredAnswers--;
+            answersUI.Decrement();
+
             if(requiredAnswers <= 0)
             {
                 //nextLevel();
@@ -33,6 +79,8 @@ public class GameSystem : MonoBehaviour
         else //false answer
         {
             lives--;
+            livesUI.Decrement();
+
             if (lives <= 0) //no more lives
                 gameOver();
         }
