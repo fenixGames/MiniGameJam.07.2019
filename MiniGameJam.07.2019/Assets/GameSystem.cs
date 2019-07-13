51e68bf5503f0e2472468737207c125de1c1bf7a
lara <larafercue@gmail.com>
2019-07-13 14:22:31 +0200

    Adding the game system interaction for the game over

---
+++ MiniGameJam.07.2019/Assets/GameSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSystem : MonoBehaviour
+{
+    public int numberOfAttempts = 3;
+    private int numberOfSuccessfulAttempts = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberOfSuccessfulAttempts = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.numberOfAttempts <= 0)
+            this.gameOver();
+    }
+
+    void gameOver()
+    {
+
+    }
+}
