1899abeb6715dadb37906cde644b9a86b349e0e7
Oussama Bouanani <oussama.bouanani@protonmail.com>
2019-07-13 16:55:51 +0200

    Add correct/wrong answers audio clips and their triggers

--- MiniGameJam.07.2019/Assets/Audio/AudioManager.cs
+++ MiniGameJam.07.2019/Assets/Audio/AudioManager.cs
@@ -6,16 +6,31 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance = null;
 
+    [SerializeField]
+    private AudioClip musicLoop = null;
+    [SerializeField]
+    private AudioSource musicSource = null;
+
     private AudioSource source;
 
     private void Awake()
     {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(instance);
+
         source = gameObject.GetComponent<AudioSource>();
+
+        //play the music loop for the level
+        musicSource.clip = musicLoop;
+        musicSource.Play();
     }
 
     public void PlayAudio (AudioClip clip)
     {
-        source.PlayOneShot(clip);
+        source.clip = clip;
+        source.Play();
     }
 
 }
